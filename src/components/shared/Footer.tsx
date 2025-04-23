eab043df17c16e7f5b38fcbae9c16f3b86bb4b93
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2025-04-23 22:19:03 +0500

    Setting up color along with header and footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Footer(){
     return (
-        <footer>
+        <footer className="bg-bg py-1.5">
             <div className="container">
-                <div className="flex items-center justify-center gap-1 text-center">
+                <div className="flex items-center justify-center gap-1 text-dark text-center">
                     All rights reserved by 
-                    <Link href={"/"} className="font-secondary font-bold text-[1.25rem] transition-all delay-300 ease-in hover:text-red-800">The Daily News.</Link>
+                    <Link href={"/"} className="font-secondary font-bold text-[1.25rem] text-dark ">The Daily News.</Link>
                     <span className="h-5">❤️</span>
                 </div>
             </div>
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,22 +3,28 @@ import Link from "next/link";
 
 export default function Navbar(){
     return (
-        <header className="bg-gray-400 py-3">
+        <header className="bg-bg py-3">
             <div className="container">
                 <div className="flex justify-between items-center">
-                    <Link href={"/"} aria-label="Home page line" className="font-secondary text-4xl font-bold leading-[1]">
+                    <Link href={"/"} aria-label="Home page line" className="font-secondary text-4xl font-bold leading-[1] text-dark">
                         The Daily News
                     </Link>
-                    <div className="flex items-center justify-center gap-2.5">
-                        <Link href={"/"} aria-label="favorite page link" className="flex items-center justify-center">
-                            <Image src={"/assets/images/icons/heart-regular.svg"} alt="heart-icon" width={20} height={20} className="w-5 object-contain" />
+                    <div className="flex items-center justify-center gap-5">
+                        <Link href={"/"} aria-label="favorite page link" className="flex items-center justify-center gap-1 text-dark transition-all delay-300 ease-in group hover:text-primary group">
+                            <Image src={"/assets/images/icons/heart-regular.svg"} alt="heart-icon" width={20} height={20} className="w-5 object-contain  transition-all delay-300 ease-in group-hover:filter-(--filter-primary)" />
                             Favorite
                         </Link>
-                        <button>
-                            <Image src={"/assets/images/icons/moon.svg"} alt="moon-icon" width={20} height={20} className="w-5 object-contain" />
+                        <button className="w-8 h-8 flex items-center justify-center border-[1px] border-dark rounded-md cursor-pointer transition-all delay-300 ease-in group hover:border-primary">
+                            <Image src={"/assets/images/icons/moon.svg"} alt="moon-icon" width={20} height={20} className="w-5 object-contain transition-all delay-300 ease-in group-hover:filter-(--filter-primary)" />
                             <Image src={"/assets/images/icons/sun.svg"} alt="sun-icon" width={20} height={20} className="hidden w-5 object-contain"  />
                         </button>
-                        {/* HERE WILL DROPDOWN COME WITH MULTIPLY LANGUAGE SUPPORT */}
+                        <select name="multi-language" className="border-[1px] border-dark rounded-sm px-1 focus:outline-[1px] focus:outline-dark text-dark" defaultValue={"English"}>
+                            <option value="English" className="text-dark">English</option>
+                            <option value="French" className="text-dark">French</option>
+                            <option value="Arabic" className="text-dark">Arabic</option>
+                            <option value="Japanese" className="text-dark">Japanese</option>
+                            <option value="Spanish" className="text-dark">Spanish</option>
+                        </select>
                     </div>
                 </div>
             </div>
